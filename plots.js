fcf7de1eafe1938e96c446dd62cec7f5b1130af6
AbsoluteMorty <85866612+AbsoluteMorty@users.noreply.github.com>
2021-11-21 02:42:54 -0600

    before challenge

--- plots.js
+++ plots.js
@@ -1,28 +1,37 @@
-Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
-var trace = {
-    x: ["burrito", "pizza", "chicken"],
-    y: [10, 18, 5],
-    type: "pie"
-};
-var layout = {
-    title: "Luncheon Survey",
-    xaxis: { title: "Food Option" },
-    yaxis: { title: "Number of Respondents" }
-};
-Plotly.newPlot("plotArea", [trace], layout);
-var trace = {
-    labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
-        "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: 'pie'
-};
-var data = [trace];
-var layout = {
-    title: "'Pie' Chart",
-};
-Plotly.newPlot("plotArea", data, layout);
-var numbers = [1, 2, 3, 4, 5];
-var doubled = numbers.map(function (num) {
-    return num * 2;
-});
-console.log(doubled);
+function init() {
+    var selector = d3.select("#selDataset");
+
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+    })
+}
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+        PANEL.append("h6").text("ID: " + result.id);
+        PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
+        PANEL.append("h6").text("Gender: " + result.gender);
+        PANEL.append("h6").text("Age: " + result.age);
+        PANEL.append("h6").text("Location: " + result.location);
+        PANEL.append("h6").text("BBtype: " + result.bbtype);
+        PANEL.append("h6").text("Wfreq: " + result.wfreq);
+    });
+}
+
+init();
