7d73d2ee3d0a14dbbd73da8d1ee81506b8b03571
AbsoluteMorty <85866612+AbsoluteMorty@users.noreply.github.com>
2021-11-21 04:37:47 -0600

    challenge

--- Static/js/charts.js
+++ Static/js/charts.js
@@ -27,7 +27,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
-  
+
 }
 
 // Demographics Panel 
@@ -58,30 +58,43 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
-
+    var result = resultArray[0];
+    
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    otu_ids = result.otu_ids;
+    otu_labels = result.otu_labels;
+    sample_values = result.sample_values;
+    console.log(result)
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
+    
 
-    var yticks = 
+    var yticks = otu_ids.sort((a, b) => a - b).slice(0,11).reverse();
+    console.log(yticks)
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
+    var barData = {
+      x: sample_values,
+      y: yticks,
+      // width: [10,10,10,10,10,10,10,10,10,10],
       
-    ];
+      label: otu_ids,
+      type: "bar",
+      orientation: "h"
+    };
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
+      barmode: "stack"
+      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar",[barData], barLayout);
   });
 }
--- Static/js/plots.js
+++ Static/js/plots.js
@@ -14,7 +14,7 @@ function init() {
 }
 function optionChanged(newSample) {
     buildMetadata(newSample);
-    buildCharts(newSample);
+    // buildCharts(newSample);
 }
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
@@ -33,5 +33,17 @@ function buildMetadata(sample) {
         PANEL.append("h6").text("Wfreq: " + result.wfreq);
     });
 }
+var trace = {
+    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    type: "bar"
+};
+var data = [trace];
+var layout = {
+    title: "'Bar' Chart",
+    xaxis: { title: "Drinks" },
+    yaxis: { title: "% of Drinks Ordered" }
+};
+Plotly.newPlot("bar", data, layout);
 
 init();
